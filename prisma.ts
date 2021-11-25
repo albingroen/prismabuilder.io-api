9d5970ebbebe6f0466051862a28a9f8cb3d467ab
Albin Groen <albin.groen@gmail.com>
2021-11-25 23:59:54 +0100

    :fire: Remove unused code

--- prisma.ts
+++ prisma.ts
@@ -79,31 +79,3 @@ export const jsonToPrismaSchema = (jsonSchema: any) => {
 
   return prismaSchema.join("\n");
 };
-
-export const PRISMA_DEFAULT_VALUE_FNS = [
-  {
-    value: "autoincrement()",
-    label: "Automatic incremenetation",
-    description: "Automatically increment each row from 0",
-  },
-  {
-    value: "uuid()",
-    label: "A random UUID",
-    description: "Automatically generate a random UUID",
-  },
-  {
-    value: "now()",
-    label: "Current date",
-    description: "The current date once the row is inserted",
-  },
-  {
-    value: "false",
-    label: "False",
-    description: "",
-  },
-  {
-    value: "true",
-    label: "True",
-    description: "",
-  },
-];
