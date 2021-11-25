67be70860010127487af8db922834b3f5bade7fa
Albin Groen <albin.groen@gmail.com>
2021-11-25 23:47:07 +0100

    :bug: Add missing dotenv lib

--- index.ts
+++ index.ts
@@ -1,7 +1,10 @@
 import bodyParser from "body-parser";
 import cors from "cors";
+import dotenv from "dotenv";
 import express from "express";
 import { format } from "@prisma/sdk";
+
+dotenv.config();
 
 const app = express();
 
